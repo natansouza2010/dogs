ba8b0f6d2bec6332cdd60d4f15f5335b8fda5548
Natan Souza <natansouza2010@gmail.com>
2022-09-05 08:36:13 -0300

    Estilizacao comentário

---
+++ src/components/Photo/PhotoDelete.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './PhotoDelete.module.css'
+
+const PhotoDelete = () => {
+  return (
+    <div>PhotoDelete</div>
+  )
+}
+
+export default PhotoDelete
